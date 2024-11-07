8cb3cd609158d07c1600a36e6cdad705420efbaa
Severius <sora0001@stud.kea.dk>
2024-11-07 15:50:34 +0100

    Buttons funktion med flere pages.

--- src/main/java/home/grupper6madspild/Controllers/Controllers.java
+++ src/main/java/home/grupper6madspild/Controllers/Controllers.java
@@ -6,8 +6,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Controllers {
 
+    // Home page
     @GetMapping("/")
     public String homePage() {
         return "homePage";
+    }
+
+    // Forbruger pages
+    @GetMapping("/forbruger")
+    public String forbruger() {
+        return "forbruger";
+    }
+    @GetMapping("/forbruger/opskrifter")
+    public String opskrifter() {
+        return "opskrifter";
+    }
+    @GetMapping("/forbruger/tips")
+    public String tips() {
+        return "tips";
+    }
+
+    // Virksomhed pages
+    @GetMapping("/virksomhed")
+    public String virksomhed() {
+        return "virksomhed";
+    }
+    @GetMapping("/virksomhed/sporing")
+    public String sporing() {
+        return "sporing";
+    }
+    @GetMapping("/virksomhed/donation")
+    public String donation() {
+        return "donation";
+    }
+
+    // Organisation pages
+    @GetMapping("/organisation")
+    public String organisation() {
+        return "organisation";
+    }
+    @GetMapping("/organisation/madspildgård")
+    public String madspildgård() {
+        return "madspildgård";
+    }
+    @GetMapping("/organisation/madspildhjemløse")
+    public String madspildhjemløse() {
+        return "madspildhjemløse";
     }
 }
