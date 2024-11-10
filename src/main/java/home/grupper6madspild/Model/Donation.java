c3538859783cbbc413aecb07ff377337e60bdb23
SeveriusRasmussen <sora0001@stud.kea.dk>
2024-11-11 00:36:58 +0100

    Donation ENDELIG FÆRDIG!

---
+++ src/main/java/home/grupper6madspild/Model/Donation.java
@@ -0,0 +1,22 @@
+package home.grupper6madspild.Model;
+
+public class Donation {
+    private String companyName;
+    private String message;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
